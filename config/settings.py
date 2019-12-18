4e1d067ad14224caed847645011d0d1133113995
Yuki Sakumoto <sakumoto@headjapan.com>
2019-12-18 17:52:15 +0900

    Installed some python modules.

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'django_filters',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
